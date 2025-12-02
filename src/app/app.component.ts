6e52aab710d910c40f273329699b3b69ec3a6093
Luís Felipe Morais <149626850+Luiissfelipe@users.noreply.github.com>
2025-12-02 16:17:51 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapViewerComponent } from './map-viewer/map-viewer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapViewerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/map-viewer/map-viewer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-viewer',
+  imports: [],
+  templateUrl: './map-viewer.component.html',
+  styleUrl: './map-viewer.component.css'
+})
+export class MapViewerComponent {
+
+}
